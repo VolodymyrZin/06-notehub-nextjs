45b8b9130e71c3826507d2b1ac6d85ab6874de6b
ZinVolodymyr <vl.zinych@gmail.com>
2026-07-29 22:50:01 +0300

    fix Link

--- app/notes/Notes.client.tsx
+++ app/notes/Notes.client.tsx
@@ -33,6 +33,7 @@ export default function NotesClient() {
     queryKey: ['notes', page, debouncedSearchQuery],
     queryFn: () => fetchNotes({ page, search: debouncedSearchQuery }),
     placeholderData: keepPreviousData,
+    refetchOnMount: false,
   });
 
   const totalPages = data?.totalPages ?? 0;
